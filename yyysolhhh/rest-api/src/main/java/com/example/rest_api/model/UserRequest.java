315a8360a9c2b4c01c061435323ac3829a45270d
yyysolhhh <solbaram37@gmail.com>
2025-02-26 16:04:47 +0900

    ch03-02. Spring Boot의 다양한 기능 살펴보기

--- yyysolhhh/rest-api/src/main/java/com/example/rest_api/model/UserRequest.java
+++ yyysolhhh/rest-api/src/main/java/com/example/rest_api/model/UserRequest.java
@@ -1,21 +1,66 @@
 package com.example.rest_api.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
+//기본 형태를 가장 추천
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+//@Setter
+//@Getter
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class UserRequest {
+//    @JsonProperty("user_names")
     private String userName;
 
+//    @JsonProperty("user_age")
     private Integer userAge; //reference 타입 사용하기
 
+//    @JsonProperty("user_email") //이름 변경
+//    @JsonProperty("email")
     private String email;
 
-    private Boolean isKorean; // is_korean
+//    @JsonProperty("is_korean")
+    private Boolean isKorean; //is_korean
+
+//    private UserRequest() {
+//
+//    }
+
+//    public void setUserNames(String name) { // set - 특정 변수와 매핑하 set 메소드 호출됨
+//        this.userName = name;
+//    }
+
+/*
+    public String getName() {
+        return this.userName;
+    }
+
+    public int getHumanAge() {
+        return this.userAge;
+    }
+*/
+
+/*
+    @JsonIgnore
+    public String getUser() {
+        return userName;
+    }
+*/
+
+/*
+    @Override
+    public String toString() {
+        return "UserRequest{" +
+                "userName='" + userName + '\'' +
+                ", userAge=" + userAge +
+                ", email='" + email + '\'' +
+                ", isKorean=" + isKorean +
+                '}';
+    }
+*/
 }
--- yyysolhhh/rest-api/src/test/java/com/example/rest_api/RestApiApplicationTests.java
+++ yyysolhhh/rest-api/src/test/java/com/example/rest_api/RestApiApplicationTests.java
@@ -1,13 +1,32 @@
 package com.example.rest_api;
 
+import com.example.rest_api.model.UserRequest;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class RestApiApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	@Autowired //스프링에서 관리하는 빈들 중에 자동으로 생성되는 오브젝트 매퍼를 가져옴
+	private ObjectMapper objectMapper;
 
+	@Test
+	void contextLoads() throws JsonProcessingException {
+//		var user = new UserRequest("홍길동", 10, "hong@gmail.com", true);
+//		System.out.println("user = " + user);
+//		user.setUserName("홍길동");
+//		user.setUserAge(10);
+//		user.setEmail("hong@gmail.com");
+//		user.setIsKorean(true);
+
+//		var json = objectMapper.writeValueAsString(user); // 직렬화
+		var json = "{\"user_names\":\"홍길동\",\"user_age\":10,\"email\":\"hong@gmail.com\",\"is_korean\":true}\n";
+		System.out.println(json);
+
+		var dto = objectMapper.readValue(json, UserRequest.class);
+		System.out.println(dto);
+	}
 }
