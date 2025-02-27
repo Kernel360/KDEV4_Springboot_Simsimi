6a9d3795b2a6225dafb41f78a17663506f1c2b09
yyysolhhh <solbaram37@gmail.com>
2025-02-27 11:11:45 +0900

    ch03-03. Spring Boot 에서의 예외처리 소개 - 2

--- yyysolhhh/exception/src/main/java/com/example/exception/exception/RestApiExceptionHandler.java
+++ yyysolhhh/exception/src/main/java/com/example/exception/exception/RestApiExceptionHandler.java
@@ -1,12 +1,14 @@
 package com.example.exception.exception;
 
+import com.example.exception.controller.RestApiController;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @Slf4j
-@RestControllerAdvice //rest api가 사용하는 곳에 예외가 일어나는 것을 감지하게 됨
+//@RestControllerAdvice(basePackages = "com.example.exception.controller") //rest api가 사용하는 곳에 예외가 일어나는 것을 감지하게 됨. basepackage 하위에 적용
+@RestControllerAdvice(basePackageClasses = { RestApiController.class, RestApiBController.class }) //둘다 지정 가능
 public class RestApiExceptionHandler {
 
     @ExceptionHandler(value = {Exception.class}) //어떤 예외 캐치할 것인지
